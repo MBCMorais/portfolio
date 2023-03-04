2cc847ddea4bac5d1d2e2e5d60da0bcd44a3e9ce
Marcelo Morais <kl4usmik4elsonn@gmail.com>
2023-03-04 20:16:45 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+import cors from 'cors';
+import { createTransport } from 'nodemailer';
+import express from 'express';
+const router = express.Router();
+
+
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+app.use('/', router);
+app.listen(5000, () => console.log('Server Running'));
+
+const contactEmail = createTransport({
+	service: process.env.EMAIL_SERVICE,
+	auth: {
+		user: process.env.EMAIL_USERNAME,
+		pass: process.env.EMAIL_PASSWORD,
+	},
+});
+
+
+
+contactEmail.verify((error) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Ready to Send');
+	}
+});
+
+router.post('/contact', (req, res) => {
+	const name = req.body.name
+	const email = req.body.email;
+	const message = req.body.message;
+	const mail = {
+		from: name,
+		to: process.env.EMAIL_TO,
+		subject: 'Contact Form Submission - Portfolio',
+		html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+	};
+	contactEmail.sendMail(mail, (error) => {
+		if (error) {
+			res.json(error);
+		} else {
+			res.json({ code: 200, status: 'Message Sent' });
+		}
+	});
+});
