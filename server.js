7c9fa9799c1a30b0159e9d0aacacab3d7aaa2a03
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2023-03-05 21:54:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import { createTransport } from 'nodemailer';
 import express from 'express';
 const router = express.Router();
 
-
+const path = require('path');
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
+
+app.use(express.static(path.resolve(__dirname, './build')));
 
 app.use('/', router);
 app.listen(5000, () => console.log('Server Running'));
@@ -22,8 +24,6 @@ const contactEmail = createTransport({
 	},
 });
 
-
-
 contactEmail.verify((error) => {
 	if (error) {
 		console.log(error);
@@ -33,7 +33,7 @@ contactEmail.verify((error) => {
 });
 
 router.post('/contact', (req, res) => {
-	const name = req.body.name
+	const name = req.body.name;
 	const email = req.body.email;
 	const message = req.body.message;
 	const mail = {
